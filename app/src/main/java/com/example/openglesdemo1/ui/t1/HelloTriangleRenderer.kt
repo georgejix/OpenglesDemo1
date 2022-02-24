7cfb70bcb9fd81535ba9c98f5b4096241b71fcab
jx <jixiao@guiji.ai>
2022-02-24 16:35:48 +0800

    彩色三角形

--- app/src/main/java/com/example/openglesdemo1/ui/t1/HelloTriangleRenderer.kt
+++ app/src/main/java/com/example/openglesdemo1/ui/t1/HelloTriangleRenderer.kt
@@ -14,14 +14,23 @@ class HelloTriangleRenderer(var context: Context) : GLSurfaceView.Renderer {
     // Member variables
     private var mProgramObject: Int = 0
     private var mVertices: FloatBuffer
+    private var mColorBuf: FloatBuffer
     private val TAG = "HelloTriangleRenderer"
     private val mVerticesData =
         floatArrayOf(0.0f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f)
+    private val mColorData = floatArrayOf(
+        0.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f, 1.0f
+    )
 
     init {
         mVertices = ByteBuffer.allocateDirect(mVerticesData.size * 4)
             .order(ByteOrder.nativeOrder()).asFloatBuffer();
         mVertices.put(mVerticesData).position(0);
+        mColorBuf = ByteBuffer.allocateDirect(mColorData.size * 4)
+            .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        mColorBuf.put(mColorData).position(0);
     }
 
     ///
@@ -58,19 +67,23 @@ class HelloTriangleRenderer(var context: Context) : GLSurfaceView.Renderer {
     //
     override fun onSurfaceCreated(gl: GL10?, config: javax.microedition.khronos.egl.EGLConfig?) {
         val vShaderStr = """#version 300 es 			  
-        layout (location = 0) in vec4 vPosition;          
+        layout (location = 0) in vec4 vPosition;   
+        layout (location = 1) in vec4 aColor;
+        out vec4 vColor;
         void main()                  
         {                            
            gl_Position = vPosition;  
            gl_PointSize = 10.0;
+           vColor = aColor;
         }                            
         """
         val fShaderStr = """#version 300 es		 			          	
-        precision mediump float;					  	
+        precision mediump float;	
+        in vec4 vColor;
         out vec4 fragColor;	 			 		  	
         void main()                                  
         {                                            
-          fragColor = vec4 ( 0.5, 0.5, 0.5, 1.0 );	
+          fragColor = vColor;	
         }                                            
         """
         val linked = IntArray(1)
@@ -125,6 +138,11 @@ class HelloTriangleRenderer(var context: Context) : GLSurfaceView.Renderer {
         GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 0, mVertices)
         //启用顶点的句柄
         GLES30.glEnableVertexAttribArray(0)
+
+        //绘制三角形颜色
+        GLES30.glEnableVertexAttribArray(1);
+        GLES30.glVertexAttribPointer(1, 4, GLES30.GL_FLOAT, false, 0, mColorBuf)
+
         //绘制三角形
         GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, 3)
         //绘制三个点
@@ -132,6 +150,9 @@ class HelloTriangleRenderer(var context: Context) : GLSurfaceView.Renderer {
         //绘制直线
         /*GLES30.glDrawArrays(GLES30.GL_LINE_STRIP, 0, 2)
         GLES30.glLineWidth(10f);*/
+
+        GLES30.glDisableVertexAttribArray(0)
+        GLES30.glDisableVertexAttribArray(1)
     }
 
     // /
