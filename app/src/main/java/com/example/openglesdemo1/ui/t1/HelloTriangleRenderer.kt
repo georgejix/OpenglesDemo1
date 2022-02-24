79c34fef3654ac9539f2fe223a4f17670bd1e052
jx <jixiao@guiji.ai>
2022-02-24 17:37:47 +0800

    三角形，圆点，直线

--- app/src/main/java/com/example/openglesdemo1/ui/t1/HelloTriangleRenderer.kt
+++ app/src/main/java/com/example/openglesdemo1/ui/t1/HelloTriangleRenderer.kt
@@ -66,25 +66,25 @@ class HelloTriangleRenderer(var context: Context) : GLSurfaceView.Renderer {
     // Initialize the shader and program object
     //
     override fun onSurfaceCreated(gl: GL10?, config: javax.microedition.khronos.egl.EGLConfig?) {
-        val vShaderStr = """#version 300 es 			  
-        layout (location = 0) in vec4 vPosition;   
+        val vShaderStr = """#version 300 es
+        layout (location = 0) in vec4 vPosition;
         layout (location = 1) in vec4 aColor;
         out vec4 vColor;
-        void main()                  
-        {                            
-           gl_Position = vPosition;  
+        void main()
+        {       
+           gl_Position = vPosition;
            gl_PointSize = 10.0;
            vColor = aColor;
-        }                            
+        }
         """
-        val fShaderStr = """#version 300 es		 			          	
-        precision mediump float;	
+        val fShaderStr = """#version 300 es
+        precision mediump float;
         in vec4 vColor;
-        out vec4 fragColor;	 			 		  	
-        void main()                                  
-        {                                            
-          fragColor = vColor;	
-        }                                            
+        out vec4 fragColor;
+        void main()
+        {
+          fragColor = vColor;
+        }
         """
         val linked = IntArray(1)
 
