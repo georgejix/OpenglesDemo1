7bf825ee6e41dbd8e8ed566c7a3792387c6e9ab0
jx <jixiao@guiji.ai>
2022-04-13 13:52:04 +0800

    convert java2kotlin

---
+++ app/src/main/java/com/example/openglesdemo1/ffmpeg/FfmpegUtil.kt
@@ -0,0 +1,17 @@
+package com.example.openglesdemo1.ffmpeg
+
+class FfmpegUtil {
+    init {
+        System.loadLibrary("ffmpegTest")
+        System.loadLibrary("avcodec")
+        System.loadLibrary("avfilter")
+        System.loadLibrary("avformat")
+        System.loadLibrary("avutil")
+        System.loadLibrary("postproc")
+        System.loadLibrary("swresample")
+        System.loadLibrary("swscale")
+    }
+
+    external fun initVideo(outputPath: String?): Int
+    external fun stopVideo(): Int
+}
