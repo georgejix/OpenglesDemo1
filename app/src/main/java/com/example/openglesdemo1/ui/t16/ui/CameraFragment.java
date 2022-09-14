69dc3f889c62a892d8df70d68d739ef1ae49c0ed
jx <jixiao@guiji.ai>
2022-09-14 10:37:34 +0800

    gl预览camera并保存视频

--- app/src/main/java/com/example/openglesdemo1/ui/t16/ui/CameraFragment.java
+++ app/src/main/java/com/example/openglesdemo1/ui/t16/ui/CameraFragment.java
@@ -1,26 +1,4 @@
 package com.example.openglesdemo1.ui.t16.ui;
-/*
- * AudioVideoRecordingSample
- * Sample project to cature audio and video from internal mic/camera and save as MPEG4 file.
- *
- * Copyright (c) 2014-2015 saki t_saki@serenegiant.com
- *
- * File name: CameraFragment.java
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- * All files in the folder are under this Apache License, Version 2.0.
- */
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -29,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
