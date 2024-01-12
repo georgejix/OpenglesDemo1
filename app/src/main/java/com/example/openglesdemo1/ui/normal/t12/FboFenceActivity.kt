b064f4db2e75cb27e2f6691d82c59f39b14f7323
jx <jix@nanjingdadao.com>
2024-01-12 19:35:26 +0800

    commit

--- app/src/main/java/com/example/openglesdemo1/ui/normal/t12/FboFenceActivity.kt
+++ app/src/main/java/com/example/openglesdemo1/ui/normal/t12/FboFenceActivity.kt
@@ -15,4 +15,10 @@ class FboFenceActivity : Activity() {
         glSurfaceView.setRenderer(SampleFenceSyncRenderer(findViewById(R.id.image_view)))
         glSurfaceView.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
     }
+
+    /**
+     * frame buffer本身并没有什么实际内容，它是通过将它的各种attachment给绑定相应的对象而实现相应的功能，
+     * 对应渲染内容来说，就是color attachment，可以通过glFramebufferTexture2D()将texture绑定到color
+     * attachment上，这时绑定这个frame buffer进行渲染，就会渲染到绑定的texture。
+     */
 }
